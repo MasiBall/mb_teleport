31791b3b1311578da9c8d77d7f64ca5481fdc439
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-11-07 13:18:36 +0200

    testi homma

--- server/server.lua
+++ server/server.lua
@@ -1,14 +1,16 @@
 local serverconfig = {
 	Teleport1 = {
-        Position = vector3(902.25885009766,-1672.7186279297,47.357490539551),
-        TargetPosition = vector3(124.25885009766,-123.7186279297,12.357490539551),
-        Heading = 160,
+        Position = vector3(-2090.63, 2638.05, 3.08),
+        TargetPosition = vector3(-2076.13, 2609.69, 7.11),
+        Heading = 15.65,
+        TargetHeading = 209.17,
         hidden = false
 	},
     TeleportTestXD = {
         Position = vector3(243.25885009766,-123.7186279297,123.357490539551),
         TargetPosition = vector3(123.25885009766,-432.7186279297,34.357490539551),
         Heading = 160,
+        TargetHeading = 209.17,
         hidden = true
 	}
 }
