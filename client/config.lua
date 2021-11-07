546a7c31f85bb4bf5fc68919253403abdb374a6a
MasiBall <76224425+MasiBall@users.noreply.github.com>
2021-11-07 15:30:04 +0200

    Update config.lua

--- client/config.lua
+++ client/config.lua
@@ -4,7 +4,7 @@ Config.NotificationText = 'Press ~INPUT_PICKUP~ to teleport'
 Config.KeyToTeleport = 38 -- Key to go through teleport
 
 -- Marker Stuff
-Config.MarkerDrawDistance = 25
+Config.MarkerDrawDistance = 8.0
 Config.MarkerType = 27
 Config.MarkerSize = {x = 2.0, y = 2.0, z = 1.0}
 Config.MarkerColor = {r = 223, g = 37, b = 37}
